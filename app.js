92abaee4dda13e50bbe21322973ad3f4fe7c6b6d
gauravqwert <100458636+gauravqwert@users.noreply.github.com>
2025-03-04 15:32:22 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,3 @@
-/* Created and coded by Abhilash Narayan */
-/* Quiz source: w3schools.com */
-
 var quiz = { "JS" : [
 	{
 		"id" : 1,
@@ -364,12 +361,12 @@ var quiz = { "JS" : [
 		"id" : 28,
 		"question" : "Which of the following is an example of an empty element?",
 		"options" : [
-			{"a": "<img/>", 
-			 "b":"<img> </img >",
-			 "c":"</img>"
+			{"a": "< img />", 
+			 "b":"< img > < /img >",
+			 "c":"< /img >"
 			}
 			],
-		"answer":"<img/>",
+		"answer":"< img />",
 		"score":0,
 		"status": ""
 	},
@@ -703,135 +700,149 @@ var quiz = { "JS" : [
 	]
 }
 
+var quizApp = function () {
+    this.score = 0;
+    this.qno = 1;
+    this.currentque = 0;
+    var totalque = quiz.JS.length;
 
+    // Timer variables
+    var timeLimit = 30 * 60; // 30 minutes in seconds
+    var timerInterval;
 
-var quizApp = function() {
+    // Function to start the countdown timer
+    this.startTimer = function () {
+        timerInterval = setInterval(function () {
+            var minutes = Math.floor(timeLimit / 60);
+            var seconds = timeLimit % 60;
 
-	this.score = 0;		
-	this.qno = 1;
-	this.currentque = 0;
-	var totalque = quiz.JS.length;
+            // Display the timer
+            document.getElementById('countdown').textContent =
+                minutes.toString().padStart(2, '0') + ':' + seconds.toString().padStart(2, '0');
 
-	
-	this.displayQuiz = function(cque) {
-		this.currentque = cque;
-		if(this.currentque <  totalque) {
-			$("#tque").html(totalque);
-			$("#previous").attr("disabled", false);
-			$("#next").attr("disabled", false);
-			$("#qid").html(quiz.JS[this.currentque].id + '.');
-	
-			
-			$("#question").html(quiz.JS[this.currentque].question);	
-			 $("#question-options").html("");
-			for (var key in quiz.JS[this.currentque].options[0]) {
-			  if (quiz.JS[this.currentque].options[0].hasOwnProperty(key)) {
-		
-				$("#question-options").append(
-					"<div class='form-check option-block'>" +
-					"<label class='form-check-label'>" +
-							  "<input type='radio' class='form-check-input' name='option'   id='q"+key+"' value='" + quiz.JS[this.currentque].options[0][key] + "'><span id='optionval'>" +
-								  quiz.JS[this.currentque].options[0][key] +
-							 "</span></label>"
-				);
-			  }
-			}
-		}
-		if(this.currentque <= 0) {
-			$("#previous").attr("disabled", true);	
-		}
-		if(this.currentque >= totalque) {
-				$('#next').attr('disabled', true);
-				for(var i = 0; i < totalque; i++) {
-					this.score = this.score + quiz.JS[i].score;
-				}
-			return this.showResult(this.score);	
-		}
-	}
-	
-	this.showResult = function(scr) {
-		$("#result").addClass('result');
-		$("#result").html("<h1 class='res-header'>Total Score: &nbsp;" + scr  + '/' + totalque + "</h1>");
-		for(var j = 0; j < totalque; j++) {
-			var res;
-			if(quiz.JS[j].score == 0) {
-					res = '<span class="wrong">' + quiz.JS[j].score + '</span><i class="fa fa-remove c-wrong"></i>';
-			} else {
-				res = '<span class="correct">' + quiz.JS[j].score + '</span><i class="fa fa-check c-correct"></i>';
-			}
-			$("#result").append(
-			'<div class="result-question"><span>Q ' + quiz.JS[j].id + '</span> &nbsp;' + quiz.JS[j].question + '</div>' +
-			'<div><b>Correct answer:</b> &nbsp;' + quiz.JS[j].answer + '</div>' +
-			'<div class="last-row"><b>Score:</b> &nbsp;' + res +
-			
-			'</div>' 
-			
-			);
-			
-		}
-	}
-	
-	this.checkAnswer = function(option) {
-		var answer = quiz.JS[this.currentque].answer;
-		option = option.replace(/\</g,"&lt;")   //for <
-		option = option.replace(/\>/g,"&gt;")   //for >
-		option = option.replace(/"/g, "&quot;")
+            // If time runs out, submit the quiz
+            if (timeLimit <= 0) {
+                clearInterval(timerInterval);
+                this.submitQuiz();
+            } else {
+                timeLimit--;
+            }
+        }.bind(this), 1000); // Update every second
+    };
 
-		if(option ==  quiz.JS[this.currentque].answer) {
-			if(quiz.JS[this.currentque].score == "") {
-				quiz.JS[this.currentque].score = 1;
-				quiz.JS[this.currentque].status = "correct";
-		}
-		} else {
-			quiz.JS[this.currentque].status = "wrong";
-		}
-		
-	}	
-	 
-	this.changeQuestion = function(cque) {
-			this.currentque = this.currentque + cque;
-			this.displayQuiz(this.currentque);	
-			
-	}
-	
-}
+    // Function to submit the quiz
+    this.submitQuiz = function () {
+        clearInterval(timerInterval); // Stop the timer
+        this.showResult(this.calculateScore()); // Show the result
+    };
 
+    // Function to calculate the total score
+    this.calculateScore = function () {
+        var totalScore = 0;
+        for (var i = 0; i < totalque; i++) {
+            totalScore += quiz.JS[i].score;
+        }
+        return totalScore;
+    };
+
+    // Start the timer when the quiz loads
+    this.startTimer();
+
+    // Rest of your existing code...
+    this.displayQuiz = function (cque) {
+        this.currentque = cque;
+        if (this.currentque < totalque) {
+            $("#tque").html(totalque);
+            $("#previous").attr("disabled", false);
+            $("#next").attr("disabled", false);
+            $("#qid").html(quiz.JS[this.currentque].id + '.');
+
+            $("#question").html(quiz.JS[this.currentque].question);
+            $("#question-options").html("");
+            for (var key in quiz.JS[this.currentque].options[0]) {
+                if (quiz.JS[this.currentque].options[0].hasOwnProperty(key)) {
+                    $("#question-options").append(
+                        "<div class='form-check option-block'>" +
+                        "<label class='form-check-label'>" +
+                        "<input type='radio' class='form-check-input' name='option' id='q" + key + "' value='" + quiz.JS[this.currentque].options[0][key] + "'><span id='optionval'>" +
+                        quiz.JS[this.currentque].options[0][key] +
+                        "</span></label>"
+                    );
+                }
+            }
+        }
+        if (this.currentque <= 0) {
+            $("#previous").attr("disabled", true);
+        }
+        if (this.currentque >= totalque) {
+            $('#next').attr('disabled', true);
+            this.submitQuiz();
+        }
+    };
+
+    this.showResult = function (scr) {
+        $("#result").addClass('result');
+        $("#result").html("<h1 class='res-header'>Total Score: &nbsp;" + scr + '/' + totalque + "</h1>");
+        for (var j = 0; j < totalque; j++) {
+            var res;
+            if (quiz.JS[j].score == 0) {
+                res = '<span class="wrong">' + quiz.JS[j].score + '</span><i class="fa fa-remove c-wrong"></i>';
+            } else {
+                res = '<span class="correct">' + quiz.JS[j].score + '</span><i class="fa fa-check c-correct"></i>';
+            }
+            $("#result").append(
+                '<div class="result-question"><span>Q ' + quiz.JS[j].id + '</span> &nbsp;' + quiz.JS[j].question + '</div>' +
+                '<div><b>Correct answer:</b> &nbsp;' + quiz.JS[j].answer + '</div>' +
+                '<div class="last-row"><b>Score:</b> &nbsp;' + res + '</div>'
+            );
+        }
+    };
+
+    this.checkAnswer = function (option) {
+        var answer = quiz.JS[this.currentque].answer;
+        option = option.replace(/</g, "&lt;").replace(/>/g, "&gt;").replace(/"/g, "&quot;");
+
+        if (option == quiz.JS[this.currentque].answer) {
+            if (quiz.JS[this.currentque].score == "") {
+                quiz.JS[this.currentque].score = 1;
+                quiz.JS[this.currentque].status = "correct";
+            }
+        } else {
+            quiz.JS[this.currentque].status = "wrong";
+        }
+    };
+
+    this.changeQuestion = function (cque) {
+        this.currentque = this.currentque + cque;
+        this.displayQuiz(this.currentque);
+    };
+};
 
 var jsq = new quizApp();
-
 var selectedopt;
-	$(document).ready(function() {
-			jsq.displayQuiz(0);		
-		
-	$('#question-options').on('change', 'input[type=radio][name=option]', function(e) {
 
-			//var radio = $(this).find('input:radio');
-			$(this).prop("checked", true);
-				selectedopt = $(this).val();
-		});
-		
-			
-			 
-	});
+$(document).ready(function () {
+    jsq.displayQuiz(0);
 
-	
-	
-	
-	$('#next').click(function(e) {
-			e.preventDefault();
-			if(selectedopt) {
-				jsq.checkAnswer(selectedopt);
-			}
-			jsq.changeQuestion(1);
-	});	
-	
-	$('#previous').click(function(e) {
-		e.preventDefault();
-		if(selectedopt) {
-			jsq.checkAnswer(selectedopt);
-		}
-			jsq.changeQuestion(-1);
-	});	
+    $('#question-options').on('change', 'input[type=radio][name=option]', function (e) {
+        selectedopt = $(this).val();
+    });
 
+    $('#next').click(function (e) {
+        e.preventDefault();
+        if (selectedopt) {
+            jsq.checkAnswer(selectedopt);
+        }
+        jsq.changeQuestion(1);
+    });
+
+    $('#previous').click(function (e) {
+        e.preventDefault();
+        if (selectedopt) {
+            jsq.checkAnswer(selectedopt);
+        }
+        jsq.changeQuestion(-1);
+    });
+});
 
 
